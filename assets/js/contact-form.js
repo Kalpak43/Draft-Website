77da323b747f0886b887d050e3bd911e01978e2e
Kalpak43 <111725804+Kalpak43@users.noreply.github.com>
2023-07-23 20:24:26 +0530

    Minor Changes

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,8 +1,5 @@
+function sendMail() {
 
-
-function sendMail(e) {
-
-    e.preventDefault()
 
     let params = {
         name: document.getElementById("name").value,
@@ -11,19 +8,6 @@ function sendMail(e) {
         message: document.getElementById("message").value,
     }
 
-    const serviceID = "service_90aqxkt"
-    const templateID = "template_6r7a0xh"
-    const publicKey = "Q12qSJRV7vP2oL3E-"
-
-    emailjs.send(serviceID, templateID, params, publicKey).then(
-        res => {
-            document.getElementById("name").value = ""
-            document.getElementById("email").value = ""
-            document.getElementById("message").value = ""
-            console.log(res)
-            alert("Message sent successfully")
-        }
-    )
-        .catch(err => console.log(err))
+    console.log(params)
 }
 
